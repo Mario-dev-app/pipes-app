cd36d6de1feca85da64f4e6dfe12b05840de43cc
Mario Peralta <marioperaltadev@gmail.com>
2022-03-30 12:10:20 -0500

    Primer commit

---
+++ src/app/app-router.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BasicosComponent } from './ventas/pages/basicos/basicos.component';
+import { NumerosComponent } from './ventas/pages/numeros/numeros.component';
+import { NoComunesComponent } from './ventas/pages/no-comunes/no-comunes.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: BasicosComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'numeros',
+    component: NumerosComponent
+  },
+  {
+    path: 'no-comunes',
+    component: NoComunesComponent
+  },
+  {
+    path: 'basicos'
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ]
+})
+export class AppRouterModule { }
